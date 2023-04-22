2289161024cdb363b7ee34620378297105b15a65
Bradleykibs <Bradleykibs@gmail.com>
2023-04-22 16:47:43 +0300

    pending request

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,22 @@
+import signUpUser from "./4-user-promise";
+import uploadPhoto from "./5-photo-reject";
+
+export default function handleProfileSignup(firstName, lastName, filename) {
+  return Promise.allSettled([
+    uploadPhoto(filename),
+    signUpUser(firstName, lastName),
+  ]).then((values) => {
+    const respArray = [];
+    values.forEach((val) => {
+      if (val.status === "fulfilled") {
+        respArray.push({ status: val.status, value: val.value });
+      } else {
+        respArray.push({
+          status: val.status,
+          value: `Error: ${val.reason.mesage}`,
+        });
+      }
+    });
+    return respArray;
+  });
+}
