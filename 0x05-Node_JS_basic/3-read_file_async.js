d0e321a543f12de6825de733c9afed6374f03389
Bradkibs <Bradleykibs@gmail.com>
2023-08-21 19:31:07 +0300

    using a call back implementation

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -5,33 +5,37 @@ async function countStudents(path) {
     * if the db is not available it throws an error
     * else it logs Number of students: NUMBER_OF_STUDENTS
     * and Number of students in FIELD: 6. List: LIST_OF_FIRSTNAMES */
-  fs.readFile(path, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
-    }
-    const lines = data.trim().split('\n');
-    const headers = lines[0].split(',');
-
-    const fieldInfo = [];
-    for (let i = 1; i < lines.length; i += 1) {
-      const values = lines[i].split(',');
-      if (values.length === headers.length) {
-        const field = values[3];
-        const firstName = values[0];
-
-        let fieldObj = fieldInfo.find((item) => item.field === field);
-        if (!fieldObj) {
-          fieldObj = { field, count: 0, first_names: [] };
-          fieldInfo.push(fieldObj);
-        }
-        fieldObj.count += 1;
-        fieldObj.first_names.push(firstName);
+  // eslint-disable-next-line no-unused-vars
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        throw new Error('Cannot load the database');
       }
-    }
-    console.log(`Number of students: ${lines.length - 1}`);
-    for (const obj of fieldInfo) {
-      console.log(`Number of students in ${obj.field}: ${obj.count}. List: ${obj.first_names.join(', ')}`);
-    }
+      const lines = data.trim().split('\n');
+      const headers = lines[0].split(',');
+
+      const fieldInfo = [];
+      for (let i = 1; i < lines.length; i += 1) {
+        const values = lines[i].split(',');
+        if (values.length === headers.length) {
+          const field = values[3];
+          const firstName = values[0];
+
+          let fieldObj = fieldInfo.find((item) => item.field === field);
+          if (!fieldObj) {
+            fieldObj = { field, count: 0, first_names: [] };
+            fieldInfo.push(fieldObj);
+          }
+          fieldObj.count += 1;
+          fieldObj.first_names.push(firstName);
+        }
+      }
+      console.log(`Number of students: ${lines.length - 1}`);
+      for (const obj of fieldInfo) {
+        console.log(`Number of students in ${obj.field}: ${obj.count}. List: ${obj.first_names.join(', ')}`);
+      }
+      resolve();
+    });
   });
 }
 module.exports = countStudents;
