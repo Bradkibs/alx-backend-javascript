dc6a502e944bab482f00fa3d4539f3aac319b1d4
Bradkibs <Bradleykibs@gmail.com>
2023-08-22 23:09:18 +0300

    node server fix

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -16,17 +16,20 @@ const app = http.createServer((request, response) => {
     const databasePath = process.argv[2];
     countStudents(databasePath)
       .then((output) => {
-        response.write(`This is the list of our students\n ${output}`);
+        response.write(`This is the list of our students\n${output}`);
         response.end();
       })
       .catch((error) => {
         response.statusCode = 404;
         response.end('Cannot load the database');
+        console.error(error); // Log the error for debugging
       });
   }
 });
 
-app.listen(port, () => {});
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 module.exports = app;
 
