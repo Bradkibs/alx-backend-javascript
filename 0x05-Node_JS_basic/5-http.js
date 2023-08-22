8321f7cd44d84078e0b15aa837fd3ebbd1fbe226
Bradkibs <Bradleykibs@gmail.com>
2023-08-22 23:07:58 +0300

    express server fix export

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,33 +1,32 @@
 const http = require('http');
-
-const port = 1245;
-const hostname = '127.0.0.1';
 const countStudents = require('./3-read_file_async');
 
-const databasePath = process.argv[2];
+const port = 1245;
 
-const app = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') {
-    res.write('Hello Holberton School!');
-    res.end();
-  } else if (req.url === '/students') {
-    res.write('This is the list of our students');
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+
+  if (request.url === '/') {
+    response.write('Hello Holberton School!');
+    response.end();
+  }
+
+  if (request.url === '/students') {
+    const databasePath = process.argv[2];
     countStudents(databasePath)
       .then((output) => {
-        res.end(output);
+        response.write(`This is the list of our students\n ${output}`);
+        response.end();
       })
       .catch((error) => {
-        res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(error.message);
+        response.statusCode = 404;
+        response.end('Cannot load the database');
       });
-  } else {
-    res.writeHead(404, { 'Content-Type': 'text/plain' });
-    res.end('Not Found\n');
   }
 });
 
-app.listen(port, hostname, () => {});
+app.listen(port, () => {});
 
 module.exports = app;
+
