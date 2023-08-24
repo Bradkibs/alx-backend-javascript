e157a9d92906e368fe890452116fa4cd5916778b
Bradkibs <Bradleykibs@gmail.com>
2023-08-24 22:03:31 +0300

    trying to fix

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,18 +1,16 @@
+const { expect } = require('chai');
+const { it, describe } = require('mocha');
 const sinon = require('sinon');
-const expect = require('chai').expect;
-const Utils = require('./utils');
-const { sendPaymentRequestToApi } = require('./3-payment');
 
-describe("sendPaymentRequestToApi", () => {
-	it('should have the same math used for sendPaymentRequestToApi(100, 20) as Utils.calculateNumber("SUM", 100, 20)', () => {
-	const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-	const consoleSpy = sinon.spy(console, 'log');
-	sendPaymentRequestToApi(100, 20);
+const Utils = require('./utils.js');
+const sendPaymentRequestToApi = require('./3-payment.js');
 
-	expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
-	expect(consoleSpy.calledWith('The total is: 120')).to.be.true;
-	consoleSpy.restore();
-	calculateNumberSpy.restore();
-	
-	});
+describe('', () => {
+  it('checking if numbers round with spies', () => {
+    const checkSoy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(1, 3);
+    expect(checkSoy.calledOnce).to.be.true;
+    expect(checkSoy.calledWith('SUM', 1, 3)).to.be.true;
+    checkSoy.restore();
+  });
 });
